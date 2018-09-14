fca258eb6b859a7a8a9c2067771c0dabcb3a2fe5
pseudo <saswathcommand@gmail.com>
2018-09-14 19:08:54 +0530

    edit path

--- chatbot/wake.py
+++ chatbot/wake.py
@@ -2,9 +2,9 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import os
 
-bot = ChatBot("Jarvis")
+bot = ChatBot("YourBot")
 bot.set_trainer(ListTrainer)
-path = "/home/pseudo/Desktop/MyPy/shit-happening/chatbot/chatterbot-corpus-master/chatterbot_corpus/data/english/"
+path = "../chatterbot-corpus-master/chatterbot_corpus/data/english/"
 for files in os.listdir(path):
     
     data = open(path+files,'r').readlines()
